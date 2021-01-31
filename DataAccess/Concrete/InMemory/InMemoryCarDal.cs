1642156ffdda0e7d95faf96b7703bc68c2de33fd
KucukdogruMerve <33333935+Kucukdogru@users.noreply.github.com>
2021-01-31 22:13:48 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car { Id=1, BrandId=1, ColorId=1, DailyPrice=1500, ModelYear=2005, Description="Mercedes" },
+                new Car { Id=2, BrandId=2, ColorId=1, DailyPrice=1500, ModelYear=2005, Description="Citroen" },
+                new Car { Id=3, BrandId=3, ColorId=2, DailyPrice=1500, ModelYear=2005, Description="BMW" },
+                new Car { Id=4, BrandId=1, ColorId=2, DailyPrice=1500, ModelYear=2010, Description="Clio" }
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAllByBrand(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public List<Car> GetAllByColor(int colorId)
+        {
+            return _cars.Where(c => c.ColorId == colorId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            var carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
---
+++ Entities/Concrete/Color.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    class Color:IEntity
+    {
+        public int Id { get; set; }
+
+        public string ColorName { get; set; }
+    }
+}
